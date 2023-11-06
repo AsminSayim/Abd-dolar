a3a4592905f72afd8e2873ac0b7d3ecec1c98fcd
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2023-11-06 12:12:09 +0300

    Add files via upload

---
+++ dolar/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dolar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double dolar=Convert.ToDouble(textBox1.Text);
+            double miktar=Convert.ToDouble(textBox2.Text);
+            double toplam = dolar * miktar;
+            label3.Text = "Toplam Tutar:" + toplam;
+        }
+    }
+}
